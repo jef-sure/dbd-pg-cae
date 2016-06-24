aeaf5ad1fcbdef21e8e88eaf04cfbf27a02eb1c4
Anton Petrusevich <anton.petrusevich@lycos.com>
2016-06-24 11:48:01 +0200

    first try to seamlessly integrate DBD::Pg into EV+Coro+AnyEvent

---
+++ lib/DBIx/PgCoroAnyEvent.pm
@@ -0,0 +1,73 @@
+{
+
+	package DBIx::PgCoroAnyEvent;
+
+=head1 NAME
+ 
+DBIx::PgCoroAnyEvent - DBD::Pg + Coro + AnyEvent
+ 
+=head1 SYNOPSIS
+ 
+  use DBI;
+  $dbh = DBI->connect("dbi:Pg:dbname=$dbname", $username, $auth, { RootClass =>"DBIx::PgCoroAnyEvent",  %rest_attr});
+
+=cut
+
+}
+{
+
+	package DBIx::PgCoroAnyEvent::db;
+	use DBD::Pg ':async';
+	use base 'DBD::Pg::db';
+	use strict;
+	use warnings;
+
+	sub prepare {
+		my ($dbh, $statement, @attribs) = @_;
+		return undef if !defined $statement;
+		$attribs[0]{pg_async} = PG_ASYNC + PG_OLDQUERY_WAIT;
+		DBD::Pg::db::prepare($dbh, $statement, @attribs);
+	}
+
+	sub do {
+		my ($dbh, $statement, $attr, @params) = @_;
+		my $sth = $dbh->prepare($statement, $attr) or return undef;
+		$sth->execute(@params) or return undef;
+		my $rows = $sth->rows;
+		($rows == 0) ? "0E0" : $rows;
+	}
+}
+
+{
+
+	package DBIx::PgCoroAnyEvent::st;
+	use EV;
+	use Coro;
+	use AnyEvent;
+	use Coro::AnyEvent;
+	use base 'DBD::Pg::st';
+
+	sub execute {
+		my ($sth, @vars) = @_;
+		my $res   = $sth->SUPER::execute(@vars);
+		my $dbh   = $sth->{Database};
+		my $async = new Coro::State;
+		while (!$dbh->pg_ready) {
+			my $w;
+			$w = AnyEvent->io(
+				fh   => $dbh->{pg_socket},
+				poll => 'r',
+				cb   => sub {
+					undef $w;
+					my $new;
+					$new = new Coro::State sub {$new->transfer($async);}
+				}
+			);
+			EV::run EV::RUN_ONCE;
+		}
+		$res = $dbh->pg_result;
+		$res;
+	}
+}
+
+1;
---
+++ t/01_sleeps.t
@@ -0,0 +1,58 @@
+use lib qw'../lib lib t';
+use Test::More;
+use DBI;
+use EV;
+use Coro;
+use AnyEvent;
+use Coro::AnyEvent;
+use Time::HiRes 'time';
+
+sub db_connect {
+	DBI->connect(
+		"dbi:Pg:dbname=anton",
+		"anton", "",
+		{   AutoCommit => 1,
+			RootClass  => 'DBIx::PgCoroAnyEvent'
+		}
+	);
+}
+
+my $cv = AE::cv;
+
+ok(my $dbh = db_connect(), 'connected');
+ok(my $sth = $dbh->prepare('select pg_sleep(2)'), 'prepared');
+my $start_time = time;
+ok($sth->execute(), 'executed');
+my $duration = time - $start_time;
+ok(($duration > 1 && $duration < 3), 'slept');
+is(ref($dbh), 'DBIx::PgCoroAnyEvent::db', 'dbh class');
+is(ref($sth), 'DBIx::PgCoroAnyEvent::st', 'sth class');
+my $status   = 0;
+my $finished = 0;
+
+for my $t (1 .. 10) {
+	$finished += 1 << $t;
+}
+
+for my $t (1 .. 10) {
+	my $timer;
+	$timer = AE::timer 0.01, 0, sub {
+		ok(my $dbh = db_connect(), "connected $t");
+		ok(my $sth = $dbh->prepare('select pg_sleep(2)'), "prepared $t");
+		my $start_time = time;
+		ok($sth->execute(), "executed $t");
+		my $duration = time - $start_time;
+		ok(($duration > 1 && $duration < 3), "slept $t");
+		$status += 1 << $t;
+		if ($status == $finished) {
+			$cv->send;
+		}
+		undef $timer;
+	};
+}
+
+$cv->recv;
+
+print "total run time: " . (time - $start_time) . " sec\n";
+
+done_testing();
